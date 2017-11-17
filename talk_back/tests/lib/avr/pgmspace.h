54fdaec035ac10b8aff72f41ff26fda20ef2b149
dcollins <david.collins@gazprom-mt.com>
2017-11-17 20:30:15 +0000

    Add test lib

---
+++ talk_back/tests/lib/avr/pgmspace.h
@@ -0,0 +1,13 @@
+
+#ifndef AVR_PGMSPACE_
+#define AVR_PGMSPACE_
+
+#include "../io.h"
+
+char pgm_read_byte(const char *data);
+// char pgm_read_byte(const char *data) {
+   // return *data;
+// }
+
+#endif //AVR_PGMSPACE_
+
